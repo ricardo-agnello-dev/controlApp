6dde9a00bae88af5a1a08a29da3d5c1b4e833ec6
Ricardo <ricardo.agnello@bol.com.br>
2021-02-06 11:33:16 -0300

    start project

---
+++ ControlApp/src/main/java/com/neutronstar/controlapp/ControlAppApplication.java
@@ -0,0 +1,13 @@
+package com.neutronstar.controlapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlAppApplication.class, args);
+	}
+
+}
---
+++ ControlApp/src/test/java/com/neutronstar/controlapp/ControlAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neutronstar.controlapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
